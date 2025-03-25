656689c78e8f242c24109a7522373beb2c804d26
Chiru <hasenorc@gmail.com>
2025-03-25 19:20:00 +0530

    Update speedtest.py

--- devgagan/modules/speedtest.py
+++ devgagan/modules/speedtest.py
@@ -85,7 +85,7 @@ async def speedtest(event):
 ├ <b>Country:</b> <code>{result['client']['country']}</code>
 ├ <b>ISP:</b> <code>{result['client']['isp']}</code>
 ├ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
-╰ <b>Powered by Team SPY</b> 
+╰ <b>Powered by 🅱🅴🅰🆂🆃</b> 
 '''
     try:
         await event.reply(string_speed,file=path,parse_mode='html')
